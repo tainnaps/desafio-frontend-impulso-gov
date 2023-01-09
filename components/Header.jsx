c58f45c36e0343581d7211f280888f8a1272e0b7
Taina Santana <tainnaps@gmail.com>
2023-01-09 06:56:19 -0300

    refactor: pass click behavior to header's logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,21 @@
 import ImpulsoGovLogo from './ImpulsoGovLogo';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
-export default function Header() {
+export default function Header({ onLogoClick }) {
   return (
     <header
       className='w-full bg-blue h-28 flex items-center justify-between px-20'
     >
-      <ImpulsoGovLogo
-        width={ 80 }
-        height={ 80 }
-      />
+      <button
+        className='bg-none'
+        onClick={ onLogoClick }
+      >
+        <ImpulsoGovLogo
+          width={ 80 }
+          height={ 80 }
+        />
+      </button>
 
       <Link href='/'>
         <a
@@ -22,3 +28,7 @@ export default function Header() {
     </header>
   );
 }
+
+Header.propTypes = {
+  onLogoClick: PropTypes.func
+}.isRequired;
--- components/ImpulsoGovLogo.jsx
+++ components/ImpulsoGovLogo.jsx
@@ -1,18 +1,19 @@
 import Image from 'next/image';
+import PropTypes from 'prop-types';
 import logo from '../public/impulsoGov.svg';
 
 export default function ImpulsoGovLogo({ width, height }) {
   return (
-    <div
-      className='cursor-pointer'
-      onClick={ () => console.log('Exibe modal!') }
-    >
-      <Image
-        alt='Impulso Gov Logo'
-        src={ logo }
-        width={ width }
-        height={ height }
-      />
-    </div>
+    <Image
+      alt='Impulso Gov Logo'
+      src={ logo }
+      width={ width }
+      height={ height }
+    />
   );
 }
+
+ImpulsoGovLogo.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number
+}.isRequired;
