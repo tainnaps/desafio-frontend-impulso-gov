ca0947382c89fe2b803102cc36c6c65ab214fb43
Taina Santana <tainnaps@gmail.com>
2023-01-09 20:52:00 -0300

    chore: add jest-localstorage-mock to jest config

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  resetMocks: false
 };
 
 module.exports = createJestConfig(customJestConfig);
